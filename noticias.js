c4b9e4e4fbe77c4d43410174348e63dd6c67db72
Morganna Giovanelli <morgcontato@gmail.com>
2018-10-13 19:17:59 -0300

    Working with different urls

--- noticias.js
+++ noticias.js
@@ -1,7 +1,18 @@
 var http = require('http');
 var server = http.createServer(
     function(req, res) {
-        res.end("<html><body>Portal de Notícias</body></html>");
+
+        var categoria = req.url;
+
+        if (categoria == '/tecnologia') {
+            res.end("<html><body>Notícias de Tecnologia</body></html>");
+        } else if (categoria == '/moda') {
+            res.end("<html><body>Notícias de Moda</body></html>");
+        } else if (categoria == '/beleza') {
+            res.end("<html><body>Notícias de Beleza</body></html>");
+        } else {
+            res.end("<html><body>Portal de Notícias</body></html>");
+        }        
 });
 
 server.listen(3000);
