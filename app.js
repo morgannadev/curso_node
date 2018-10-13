11b9d461889460083c423c279698c22b548cf994
Morganna Giovanelli <morgcontato@gmail.com>
2018-10-13 20:49:17 -0300

    Refactoring modules

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 var app = require('./config/server'); // recuperando as configs do servidor (em módulo)
 
-app.get('/', function(req, res) {
-    res.render("home/index");
-});
-
-app.get('/formulario_inclusao_noticia', function(req, res) {
-    res.render("admin/form_add_noticia");
-});
-
-app.get('/noticias', function(req, res) {
-    res.render("noticias/noticias");
-});
+var rotaNoticias = require('./app/routes/noticias')(app);
+var rotaHome = require('./app/routes/home')(app);
+var rotaFormInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function() {
     console.log('Servidor rodando com Express');
--- config/server.js
+++ config/server.js
@@ -1,5 +1,6 @@
 var express = require('express'); // recuperando a biblioteca do express
 var  app = express(); // executando a função que o express retorna
 app.set('view engine', 'ejs'); // definindo o motor de geração de views - ejs
+app.set('views', './app/views'); // local a partir do nível do app.js
 
 module.exports = app;
