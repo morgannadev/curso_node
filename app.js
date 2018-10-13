0dbec1a66852aa46a2cfc83eaf8b9fa52605732f
Morganna Giovanelli <morgcontato@gmail.com>
2018-10-13 19:58:19 -0300

    Refactoring using ejs and add views

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express'); // recuperando a biblioteca do express
 var  app = express(); // executando a função que o express retorna
 
-app.get('/', function(req, res) {
-    res.send("<html><body>Portal de Notícias</body></html>");
-});
+app.set('view engine', 'ejs'); // definindo o motor de geração de views - ejs
 
 app.get('/tecnologia', function(req, res) {
-    res.send("<html><body>Notícias de Tecnologia</body></html>");
+    res.render("secao/tecnologia");
+});
+
+app.get('/', function(req, res) {
+    res.send("<html><body>Portal de Notícias</body></html>");
 });
 
 app.listen(3000, function() {
