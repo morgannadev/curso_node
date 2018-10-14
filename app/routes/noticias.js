a6c991ddf31a4065311e1b72dd1c199e0bcb2ef1
Morganna Giovanelli <morgcontato@gmail.com>
2018-10-14 00:12:24 -0300

    First script in MySQL

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,20 @@
 module.exports = function(app) {
+
     app.get('/noticias', function(req, res) {
-        res.render("noticias/noticias");
+
+        var mysql = require('mysql');
+        
+            var connection = mysql.createConnection({
+                host : 'localhost',
+                user : 'root',
+                password : '1234',
+                database : 'portal_noticias'
+            });
+        
+            connection.query('select * from noticias', function(error, result) {
+                res.send(result);
+            });
+
+        // res.render("noticias/noticias");
     });
 };
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # Curso de Node.js
 
-Repositório para curso e estudo de node.js.
+Repositório para curso em andamento e estudo de node.js.
 
 Utilizando:
 - Express: um framework node.js para aplicações web;
 - EJS: uma linguagem de modelagem para criação de páginas HTML utilizando js;
 - Nodemon: utilitário que reinicia automaticamente o servidor node.js;
 - NPM: gerenciador de pacotes js.
+
+Banco de Dados:
+- MySQL Community.
